e0b8bf2d7dd1138fa92b6ab1aecca5d955f0671d
Thomas Panas <thomas@friendhire.com>
2017-02-28 23:00:55 -0800

    Simple App

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '7.0'
+
+target 'acrobert' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for acrobert
+  pod 'SwiftyJSON', '~> 3.1'
+  pod 'MBProgressHUD', '~> 1.0.0'
+
+
+end
--- acrobert/ViewController.swift
+++ acrobert/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    let session = URLSession.shared
+
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var errorMessage: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.spinner.hidesWhenStopped = true
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
 
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton){
+        let inputStr: String = inputField.text!;
+        if (inputStr.characters.count <= 0) {
+            return
+        }
+        self.spinner.startAnimating()
+        var theURL: URL {
+            return URL(string: "https://www.nactem.ac.uk/software/acromine/dictionary.py?sf=\(inputStr)")!
+        }
+        NSLog(theURL.absoluteString)
+       
+        do {
+            let data = try Data(contentsOf: theURL)
+            let json = JSON(data: data)
+
+            var resultData:String = ""
+            if let names = json[0]["lfs"].array {
+                for name in names {
+                    if let title = name["lf"].string {
+                        resultData.append(title+"\n")
+                    }
+                }
+            }
+            textView.text = resultData
+            self.spinner.stopAnimating()
+        } catch {
+            errorMessage.text = "Error retrieving data"
+        }
+        
+    }
+    
 
 }
 
